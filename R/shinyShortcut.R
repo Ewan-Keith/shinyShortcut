a1f4f5e20749abfb69ca4964e7fdc98c7027662a
Ewan Keith <keith.ewan@yahoo.co.uk>
2017-03-13 22:37:02 +0000

    added gitignore option

--- R/shinyShortcut.R
+++ R/shinyShortcut.R
@@ -3,27 +3,39 @@
 #' Creates an executable file that, when ran, will open the co-located shiny
 #' app using the user's default internet browser.
 #'
-#' @param shinyDirectory The top-level directory containing shiny code and
-#' materials. Also where the final shortcut file will be saved.
-#' @param OS The operating system that R is being ran on.
+#' @param shinyDirectory The top-level directory containing the code and
+#' materials for the shiny application. Also where the
+#' final shortcut file will be saved.
+#' @param OS The operating system that R is being ran on. Must be one of
+#' \code{"windows"} or \code{"unix"}.
+#' @param gitIgnore If True then all produced files will be added to a
+#' \code{.gitignore} file in the specified directory (if there is no
+#' such file it will be created).
 #'
 #' @details Calling \code{shinyShortcut} will write an executable file
-#' that when called will run the shiny app in the default browser.
+#' that will run the shiny app in the user's default browser.
 #' This executable is stored in a created folder named  .shiny_run'
 #' whilst a shortcut that will silently run the executable
 #' is saved in the home directory as 'shinyShortcut' (this will
 #' be a .vbs script on windows or a .desktop file on unix systems).
+#' These files can then be linked to from other locations such as the
+#' user's desktop.
 #'
 #' @examples
 #' shinyShortcut()
 #' @export
-shinyShortcut <- function(shinyDirectory = getwd(), OS = .Platform$OS.type) {
+shinyShortcut <- function(shinyDirectory = getwd(), OS = .Platform$OS.type,
+                          gitIgnore = FALSE) {
 
   # if relevant files exist delete them first
-  unlink(paste0(shinyDirectory, "/.shiny_run"), recursive = TRUE, force = TRUE)
-  unlink(paste0(shinyDirectory, "/shiny_run.desktop"), recursive = FALSE, force = TRUE)
-  unlink(paste0(shinyDirectory, "/shinyShortcut.vbs"), recursive = FALSE, force = TRUE)
-  unlink(paste0(shinyDirectory, "/shinyShortcut.cmd"), recursive = FALSE, force = TRUE)
+  unlink(paste0(shinyDirectory, "/.shiny_run"),
+         recursive = TRUE, force = TRUE)
+  unlink(paste0(shinyDirectory, "/shiny_run.desktop"),
+         recursive = FALSE, force = TRUE)
+  unlink(paste0(shinyDirectory, "/shinyShortcut.vbs"),
+         recursive = FALSE, force = TRUE)
+  unlink(paste0(shinyDirectory, "/shinyShortcut.cmd"),
+         recursive = FALSE, force = TRUE)
 
   dir.create(paste(shinyDirectory, ".shiny_run", sep = "/"))
 
@@ -107,4 +119,11 @@ shinyShortcut <- function(shinyDirectory = getwd(), OS = .Platform$OS.type) {
     system(paste0("chmod +x ", shortcut_reference))
 
   } else stop("OS must be one of \"windows\" or \"unix\"")
+
+  # if specified, add files and folder to local .gitignore file
+  if (gitIgnore){
+    cat(c("\n.shiny_run/", "\nshinyShortcut"), sep = "",
+        file = paste0(shinyDirectory, "/.gitignore"),
+        append = TRUE)
+  }
 }
